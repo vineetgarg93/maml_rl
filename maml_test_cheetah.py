cf7975e85687f633456a0dd401bbaa19b4667a7d
Vineet Garg <vineet@ecewkgsw334d01.ece.tamu.edu>
2018-12-02 20:21:48 -0600

    main script

--- maml_test_cheetah.py
+++ maml_test_cheetah.py
@@ -17,7 +17,12 @@ import tensorflow as tf
 #stub(globals())
 
 from rllab.misc.instrument import VariantGenerator, variant
-
+from test import test_const_adv, test_learnt_adv
+import numpy as np
+import pickle
+import argparse
+import os
+import random
 
 class VG(VariantGenerator):
 
@@ -50,20 +55,67 @@ class VG(VariantGenerator):
 
 variants = VG().variants()
 
-max_path_length = 200
-num_grad_updates = 1
-use_maml=True
+## Pass arguments ##
+parser = argparse.ArgumentParser()
+parser.add_argument('--env', type=str, default='HalfCheetahTorsoAdv-v1', help='Name of adversarial environment')
+parser.add_argument('--max_path_length', type=int, default=200, help='maximum episode length')
+parser.add_argument('--layer_size', nargs='+', type=int, default=[100,100], help='layer definition')
+parser.add_argument('--if_render', type=int, default=0, help='Should we render?')
+parser.add_argument('--after_render', type=int, default=100, help='After how many to animate')
+parser.add_argument('--num_grad_updates', type=int, default=1, help='Number of training instances to run')
+parser.add_argument('--n_itr', type=int, default=800, help='Number of iterations of the alternating optimization')
+parser.add_argument('--n_pro_itr', type=int, default=1, help='Number of iterations for the portagonist')
+parser.add_argument('--n_adv_itr', type=int, default=1, help='Number of interations for the adversary')
+parser.add_argument('--fast_batch_size', type=int, default=20, help='')
+parser.add_argument('--save_every', type=int, default=10, help='Save checkpoint every save_every iterations')
+parser.add_argument('--meta_batch_size', type=int, default=40, help='')
+parser.add_argument('--adv_fraction', type=float, default=1, help='fraction of maximum adversarial force to be applied')
+parser.add_argument('--fast_lr', type=float, default=0.1, help='')
+parser.add_argument('--meta_step_size', type=float, default=0.01, help='')
+parser.add_argument('--gae_lambda', type=float, default=0.97, help='gae_lambda for learner')
+parser.add_argument('--folder', type=str, default=os.path.join(os.getcwd(),"results"), help='folder to save result in')
+
+
+## Parsing Arguments ##
+args = parser.parse_args()
+env_name = args.env
+max_path_length = args.max_path_length
+layer_size = tuple(args.layer_size)
+ifRender = bool(args.if_render)
+afterRender = args.after_render
+num_grad_updates = args.num_grad_updates
+n_itr = args.n_itr
+n_pro_itr = args.n_pro_itr
+n_adv_itr = args.n_adv_itr
+batch_size = args.fast_batch_size
+save_every = args.save_every
+meta_batch_size = args.meta_batch_size
+adv_fraction = args.adv_fraction
+step_size = args.meta_step_size
+grad_step_size = args.fast_lr
+gae_lambda = args.gae_lambda
+save_dir = args.folder
+
+
+## Initializing summaries for the tests ##
+const_test_rew_summary = []
+adv_test_rew_summary = []
+
+save_prefix = 'env-{}_Itr{}_BS{}_Adv{}_stp{}_lam{}_{}'.format(env_name, n_itr, batch_size, adv_fraction, step_size, gae_lambda, random.randint(0,1000000))
+save_name = save_dir+'/'+save_prefix+'.p'
 
 for v in variants:
-    direc = v['direc']
-    learning_rate = v['meta_step_size']
-
-    if direc:
-        # env = TfEnv(normalize(HalfCheetahEnvRandDirec()))
-        env = TfEnv(normalize(GymEnv('HalfCheetahAdv-v1', 1.0)))
-    else:
-        # env = TfEnv(normalize(HalfCheetahEnvRand()))
-        env = TfEnv(normalize(GymEnv('HalfCheetahAdv-v1', 1.0)))
+#    direc = v['direc']
+#    learning_rate = v['meta_step_size']
+#    if direc:
+#        # env = TfEnv(normalize(HalfCheetahEnvRandDirec()))
+#        env = TfEnv(normalize(GymEnv('HalfCheetahAdv-v1', 1.0)))
+#    else:
+#        # env = TfEnv(normalize(HalfCheetahEnvRand()))
+#        env = TfEnv(normalize(GymEnv('HalfCheetahAdv-v1', 1.0)))
+    
+    env = TfEnv(normalize(GymEnv(env_name, 1.0)))
+    env_orig = TfEnv(normalize(GymEnv(env_name, 1.0)))
 
     pro_policy = MAMLGaussianMLPPolicy(
         name="pro_policy",
@@ -97,7 +149,7 @@ for v in variants:
         max_path_length=max_path_length,
         meta_batch_size=v['meta_batch_size'],
         num_grad_updates=num_grad_updates,
-        n_itr=800,
+        n_itr=1,
         use_maml=False,
         step_size=v['meta_step_size'],
         plot=False,
@@ -114,7 +166,7 @@ for v in variants:
         max_path_length=max_path_length,
         meta_batch_size=v['meta_batch_size'],
         num_grad_updates=num_grad_updates,
-        n_itr=800,
+        n_itr=1,
         use_maml=True,
         step_size=v['meta_step_size'],
         plot=False,
@@ -124,14 +176,43 @@ for v in variants:
     pro_rews = []
     adv_rews = []
     all_rews = []
+    const_testing_rews = []
+#    const_testing_rews.append(test_const_adv(env_orig, pro_policy, path_length=200))
+    adv_testing_rews = []
+#    adv_testing_rews.append(test_learnt_adv(env, pro_policy, adv_policy, path_length=200))
 
-    for ni in range(10):
+
+    for ni in range(n_itr):
         logger.log('\n\n\n####global itr# {} ####\n\n\n'.format(ni))
         ## Train protagonist
         pro_algo.train()
-        # pro_rews += pro_algo.rews; all_rews += pro_algo.rews;
-        # logger.log('Protag Reward: {}'.format(np.array(pro_algo.rews).mean()))
-        ## Train Adversary
+        pro_rews += pro_algo.rews; all_rews += pro_algo.rews;
+        logger.log('Protag Reward: {}'.format(np.array(pro_algo.rews).mean()))
+#        ## Train Adversary
         adv_algo.train()
-        # adv_rews += adv_algo.rews; all_rews += adv_algo.rews;
-        # logger.log('Advers Reward: {}'.format(np.array(adv_algo.rews).mean()))
+        adv_rews += adv_algo.rews; all_rews += adv_algo.rews;
+        logger.log('Advers Reward: {}'.format(np.array(adv_algo.rews).mean()))
+#        ## Test the learnt policies
+        const_adv_reward = test_const_adv(env, pro_policy, num_tasks = v['meta_batch_size'], path_length=200)
+        logger.log('Const Advers Reward: {}'.format(const_adv_reward))
+        const_testing_rews.append(const_adv_reward)
+        
+        learned_adv_reward = test_learnt_adv(env, pro_policy, adv_policy, num_tasks = v['meta_batch_size'], path_length=200)
+        logger.log('Learned Advers Reward: {}'.format(learned_adv_reward))
+        adv_testing_rews.append(learned_adv_reward)
+
+        if ni!=0 and ni%save_every==0:
+            ## SAVING CHECKPOINT INFO ##
+            pickle.dump({'args': args,
+                         'pro_policy': pro_policy,
+                         'adv_policy': adv_policy,
+                         'zero_test': const_test_rew_summary,
+                         'iter_save': ni,
+                         'adv_test': adv_test_rew_summary}, open(save_name+"_"+str(ni)+'.temp','wb'))
+#    
+#    ## Shutting down the optimizer ##
+#    pro_algo.shutdown_worker()
+#    adv_algo.shutdown_worker()
+
+
+logger.log('\n\n\n#### DONE ####\n\n\n')
